092ca76312a38d5fde52b0f402edee30883a6a78
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-01-25 19:09:43 +0300

    Dispatch

--- Movie/src/App.jsx
+++ Movie/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { MovieInput } from './Components/MovieInput'
+import { MovieList } from './Components/MovieList'
+
 
 function App() {
 
   return (
     <>
-
+      <MovieInput />
+      <MovieList />
     </>
   )
 }
---
+++ Movie/src/Components/MovieInput.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { addMovie } from "../MovieSlice";
+import { useDispatch } from "react-redux";
+
+const MovieInput = () => {
+    const [newMovie, setNewMovie] = useState("");
+    const dispatch = useDispatch();
+
+    const handleAddMovie = () => {
+        if (!newMovie) return;
+        dispatch(addMovie(newMovie));
+        setNewMovie("");
+    };
+
+    return (
+        <>
+            <div>
+                <input
+                    type="text"
+                    value={newMovie}
+                    onChange={(e) => setNewMovie(e.target.value)}
+                />
+                <button onClick={handleAddMovie}>Add Movie</button>
+            </div>
+        </>
+    );
+};
+
+export default MovieInput;
